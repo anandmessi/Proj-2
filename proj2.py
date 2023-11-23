93b268a0e2e3f56bbc8beaee2f53077438ba9c40
Ic0nix_ <68050147+anandmessi@users.noreply.github.com>
2023-11-23 18:10:07 +0530

    Update proj2.py

--- proj2.py
+++ proj2.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox, simpledialog, filedialog
+from tkinter import ttk, messagebox, simpledialog, filedialog
 import mysql.connector
 from PIL import Image, ImageTk
 
@@ -7,7 +7,7 @@ from PIL import Image, ImageTk
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="2006",
+    password="root",
     database="car_database"  # Add this line to specify the database
 )
 
@@ -36,18 +36,40 @@ def clear_widgets(parent):
     for widget in parent.winfo_children():
         widget.destroy()
 
+class CustomDialog(tk.Toplevel):
+    def __init__(self, parent, title, options):
+        super().__init__(parent)
+        self.title(title)
+        self.geometry("300x150")
+
+        self.result = None
+
+        label = tk.Label(self, text=f"Select {title}:")
+        label.pack(pady=10)
+
+        combo_var = tk.StringVar()
+        combo = ttk.Combobox(self, textvariable=combo_var, values=options)
+        combo.pack(pady=10)
+
+        btn_ok = tk.Button(self, text="OK", command=lambda: self.ok_button(combo_var.get()))
+        btn_ok.pack(pady=10)
+
+    def ok_button(self, value):
+        self.result = value
+        self.destroy()
+
 # Function to add details
 def add_details():
     company = simpledialog.askstring("Add Details", "Enter Company Name:")
-    
+
     if company:
         car_name = simpledialog.askstring("Add Details", "Enter Car Name:")
-        
+
         if car_name:
             year = simpledialog.askinteger("Add Details", "Enter Year:")
             engine_type_menu = tk.Toplevel(root)
             engine_type_menu.title("Select Engine Type")
-            
+
             # Function to handle file selection
             def choose_image(selected_engine_type):
                 file_path = filedialog.askopenfilename(title="Select Image File", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
@@ -56,7 +78,7 @@ def add_details():
                                    (company, car_name, year, selected_engine_type, file_path))
                     db.commit()
                     messagebox.showinfo("Success", "Details added successfully!")
-            
+
             # Create buttons for engine types
             engine_types = ["Petrol", "Diesel", "Electric", "Hybrid"]
             for engine_type in engine_types:
@@ -65,21 +87,21 @@ def add_details():
 # Function to show details
 def show_details(cursor):
     clear_widgets(root)
-    
+
     cursor.execute("SELECT DISTINCT company FROM cars")
     companies = cursor.fetchall()
 
     if not companies:
         messagebox.showinfo("No Data", "No details found. Please add data first.")
         return
-    
+
     for company in companies:
         tk.Button(root, text=company[0], command=lambda comp=company[0]: show_company_models(comp, cursor)).pack(pady=5)
 
 # Function to show models of a selected company
 def show_company_models(selected_company, cursor):
     clear_widgets(root)
-    
+
     cursor.execute("SELECT DISTINCT car_name FROM cars WHERE company = %s", (selected_company,))
     car_names = cursor.fetchall()
 
@@ -89,78 +111,134 @@ def show_company_models(selected_company, cursor):
 # Function to show details of a selected car
 def show_car_details(company, car_name, cursor):
     clear_widgets(root)
-    
+
     cursor.execute("SELECT car_name, year, engine_type, image_url FROM cars WHERE company = %s AND car_name = %s",
                    (company, car_name))
     car_details = cursor.fetchall()
 
     for car in car_details:
         car_frame = tk.Frame(root, bg="white")
-        car_frame.pack(padx=20, pady=20)
+        car_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
 
-        tk.Label(car_frame, text=f"Car Name: {car[0]}, Year: {car[1]}, Engine Type: {car[2]}", bg="white").pack()
+        # Left side: Car details
+        details_frame = tk.Frame(car_frame, bg="white")
+        details_frame.pack(side=tk.LEFT, padx=20)
+
+        tk.Label(details_frame, text=f"Car Name: {car[0]}", bg="white").pack(anchor='w')
+        tk.Label(details_frame, text=f"Year: {car[1]}", bg="white").pack(anchor='w')
+        tk.Label(details_frame, text=f"Engine Type: {car[2]}", bg="white").pack(anchor='w')
+
+        # Right side: Image
+        image_frame = tk.Frame(car_frame, bg="white")
+        image_frame.pack(side=tk.RIGHT)
 
         if car[3]:
             try:
                 img = Image.open(car[3])
-                img = img.resize((200, 150), Image.BILINEAR)
+                img = img.resize((root.winfo_screenwidth() // 3, root.winfo_screenheight() // 3), Image.LANCZOS)
                 img = ImageTk.PhotoImage(img)
-                tk.Label(car_frame, image=img, bg="white").image = img
-                tk.Label(car_frame, image=img, bg="white").pack(padx=5, pady=5)
+                img_label = tk.Label(image_frame, image=img, bg="white")
+                img_label.image = img
+                img_label.pack(padx=20, pady=20, anchor='e')
             except Exception as e:
                 print("Error loading image:", e)
-# Function to edit details
-def edit_details():
-    selected_company = simpledialog.askstring("Edit Details", "Enter Company Name:")
-    
-    if selected_company:
-        selected_car = simpledialog.askstring("Edit Details", "Enter Car Name:")
-        
-        if selected_car:
-            edit_window = tk.Toplevel(root)
-            edit_window.title("Edit Car Details")
 
-            # Fetch current details for the selected car
-            cursor.execute("SELECT year, engine_type, image_url FROM cars WHERE company = %s AND car_name = %s",
-                           (selected_company, selected_car))
-            current_details = cursor.fetchone()
+        # Back button
+        back_button = tk.Button(car_frame, text="Back", command=lambda: show_models(company, cursor))
+        back_button.pack(side=tk.BOTTOM, pady=20)
 
-            if current_details:
-                current_year, current_engine_type, current_image_url = current_details
+# Function to show edit options for the selected company and car
+def show_edit_options(selected_company, selected_car):
+    edit_options_window = tk.Toplevel(root)
+    edit_options_window.title("Edit Options")
 
-                # Create entry fields with current values
-                year_var = tk.StringVar(value=current_year)
-                engine_type_var = tk.StringVar(value=current_engine_type)
-                image_url_var = tk.StringVar(value=current_image_url)
+    # Function to handle edit button click
+    def edit_button_click():
+        edit_car_details(selected_company, selected_car)
 
-                # Create labels and entry widgets for each field
-                tk.Label(edit_window, text="Year:").pack()
-                year_entry = tk.Entry(edit_window, textvariable=year_var)
-                year_entry.pack()
+    # Edit button
+    tk.Button(edit_options_window, text="Edit Car Details", command=edit_button_click).pack()
 
-                tk.Label(edit_window, text="Engine Type:").pack()
-                engine_type_entry = tk.Entry(edit_window, textvariable=engine_type_var)
-                engine_type_entry.pack()
+    # Function to handle delete button click
+    def delete_button_click():
+        delete_car(selected_company, selected_car)
 
-                tk.Label(edit_window, text="Image URL:").pack()
+    # Delete button
+    tk.Button(edit_options_window, text="Delete Car", command=delete_button_click).pack()
 
-                # Function to handle file selection
-                def choose_image():
-                    file_path = filedialog.askopenfilename(title="Select Image File", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
-                    if file_path:
-                        image_url_var.set(file_path)
+def edit_car_details(selected_company, selected_car):
+    edit_window = tk.Toplevel(root)
+    edit_window.title("Edit Car Details")
 
-                # Button to choose image file
-                tk.Button(edit_window, text="Choose Image", command=choose_image).pack()
+    # Fetch current details for the selected car
+    cursor.execute("SELECT car_name, year, engine_type, image_url FROM cars WHERE company = %s AND car_name = %s",
+                   (selected_company, selected_car))
+    current_details = cursor.fetchone()
 
-                # Function to handle save button click
-                def save_button_click():
-                    save_changes(selected_company, selected_car, year_var.get(), engine_type_var.get(), image_url_var.get())
-                    # Close the edit window
-                    edit_window.destroy()
+    if current_details:
+        current_car_name, current_year, current_engine_type, current_image_url = current_details
 
-                # Save button
-                tk.Button(edit_window, text="Save Changes", command=save_button_click).pack()
+        # Create entry fields with current values
+        car_name_var = tk.StringVar(value=current_car_name)
+        year_var = tk.StringVar(value=current_year)
+        engine_type_var = tk.StringVar(value=current_engine_type)
+        image_url_var= tk.StringVar(value=current_image_url)
+
+        # Create labels and entry widgets for each field
+        tk.Label(edit_window, text="Car Name:").pack()
+        car_name_entry = tk.Entry(edit_window, textvariable=car_name_var)
+        car_name_entry.pack()
+
+        tk.Label(edit_window, text="Year:").pack()
+        year_entry = tk.Entry(edit_window, textvariable=year_var)
+        year_entry.pack()
+
+        tk.Label(edit_window, text="Engine Type:").pack()
+        engine_type_entry = tk.Entry(edit_window, textvariable=engine_type_var)
+        engine_type_entry.pack()
+
+        tk.Label(edit_window, text="Image URL:").pack()
+
+        # Function to handle file selection
+        def choose_image():
+            file_path = filedialog.askopenfilename(title="Select Image File", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
+            if file_path:
+                image_url_var.set(file_path)
+
+        # Button to choose image file
+        tk.Button(edit_window, text="Choose Image", command=choose_image).pack()
+
+        # Function to handle save button click
+        def save_button_click():
+            new_car_name = car_name_var.get()
+            new_year = year_var.get()
+            new_engine_type = engine_type_var.get()
+            new_image_url = image_url_var.get()
+
+            # Validate that the new car name is not empty
+            if not new_car_name:
+                messagebox.showerror("Error", "Car Name cannot be empty.")
+                return
+
+            # Update the car details
+            cursor.execute("""
+            UPDATE cars
+            SET car_name = %s, year = %s, engine_type = %s, image_url = %s
+            WHERE company = %s AND car_name = %s
+            """, (new_car_name, new_year, new_engine_type, new_image_url, selected_company, selected_car))
+            db.commit()
+
+            # Show success message
+            messagebox.showinfo("Success", "Changes saved successfully!")
+
+            # Close the edit window
+            edit_window.destroy()
+
+            # Show updated details
+            show_details(cursor)
+
+        # Save button
+        tk.Button(edit_window, text="Save Changes", command=save_button_click).pack()
 
 # Function to save changes
 def save_changes(company, car_name, new_year, new_engine_type, new_image_url):
@@ -176,7 +254,22 @@ def save_changes(company, car_name, new_year, new_engine_type, new_image_url):
 
 # Function to handle delete button click
 def delete_button_click():
-    selected_company = simpledialog.askstring("Delete Company", "Enter Company Name:")
+    # Fetch distinct companies
+    cursor.execute("SELECT DISTINCT company FROM cars")
+    companies = cursor.fetchall()
+
+    if not companies:
+        messagebox.showinfo("No Data", "No companies found. Please add data first.")
+        return
+
+    # Create a list of company names
+    company_name_list = [company[0] for company in companies]
+
+    # Create a custom dialog for selecting the company
+    company_dialog = CustomDialog(root, "Company", company_name_list)
+    root.wait_window(company_dialog)
+
+    selected_company = company_dialog.result
 
     if selected_company:
         delete_company(selected_company)
@@ -195,13 +288,40 @@ def delete_company(selected_company):
 
 # Function to handle delete car button click
 def delete_car_button_click():
-    selected_company = simpledialog.askstring("Delete Car", "Enter Company Name:")
+    # Fetch distinct companies
+    cursor.execute("SELECT DISTINCT company FROM cars")
+    companies = cursor.fetchall()
+
+    if not companies:
+        messagebox.showinfo("No Data", "No companies found. Please add data first.")
+        return
+
+    # Create a list of company names
+    company_name_list = [company[0] for company in companies]
+
+    # Create a custom dialog for selecting the company
+    company_dialog = CustomDialog(root, "Company", company_name_list)
+    root.wait_window(company_dialog)
+
+    selected_company = company_dialog.result
 
     if selected_company:
-        selected_car = simpledialog.askstring("Delete Car", "Enter Car Name:")
+        # Fetch distinct car names for the selected company
+        cursor.execute("SELECT DISTINCT car_name FROM cars WHERE company = %s", (selected_company,))
+        car_names = cursor.fetchall()
 
-        if selected_car:
-            delete_car(selected_company, selected_car)
+        if car_names:
+            # Create a list of car names
+            car_name_list = [car[0] for car in car_names]
+
+            # Create a custom dialog for selecting the car
+            car_dialog = CustomDialog(root, "Car", car_name_list)
+            root.wait_window(car_dialog)
+
+            selected_car = car_dialog.result
+
+            if selected_car:
+                delete_car(selected_company, selected_car)
 
 # Function to delete a car
 def delete_car(selected_company, selected_car):
@@ -221,6 +341,7 @@ root.title("Car Database")
 
 # Load the background image
 bg_image = Image.open("background.jpg")  # Replace with your image path
+bg_image = bg_image.resize((root.winfo_screenwidth(), root.winfo_screenheight()), Image.LANCZOS)
 bg_photo = ImageTk.PhotoImage(bg_image)
 
 # Get the dimensions of the background image
@@ -236,7 +357,7 @@ bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Function to create smaller buttons
 def create_button(parent, text, command):
-    button = tk.Button(parent, text=text, command=command, width=int(bg_width * 0.01), height=int(bg_height * 0.005))
+    button = tk.Button(parent, text=text, command=command, width=int(bg_width * 0.01), height=int(bg_height * 0.005), font=("Helvetica", 12))
     return button
 
 # Create buttons
@@ -246,11 +367,8 @@ add_button.pack(pady=int(bg_height * 0.003))
 show_button = create_button(root, "Show", lambda: show_details(cursor))
 show_button.pack(pady=int(bg_height * 0.003))
 
-edit_button= create_button(root, "Edit", edit_details)
+edit_button = create_button(root, "Edit", edit_details)
 edit_button.pack(pady=int(bg_height * 0.003))
-
-delete_button = create_button(root, "Delete", delete_button_click)
-delete_button.pack(pady=int(bg_height * 0.003))
 
 delete_car_button = create_button(root, "Delete Car", delete_car_button_click)
 delete_car_button.pack(pady=int(bg_height * 0.003))
